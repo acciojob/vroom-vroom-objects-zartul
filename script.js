b2d437af41388c73a0a9cbe35a497ee1469d37a0
zartul <121471819+zartul@users.noreply.github.com>
2025-02-11 10:33:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 // Complete the js code
-function Car(make, model) {}
-
-function SportsCar(make, model, topSpeed) {}
-
+function Car(make, model) {
+	this.make=make;
+	this.model=model;
+}
+function SportsCar(make, model, topSpeed) {
+	Car.call(this,make,model);
+	this.topSpeed=topSpeed;
+}
+// class Car{
+// 	constructor(make,model){
+// 		this.make=make;
+// 		this.model=model;
+// 	}
+	
+// }
+Car.prototype.getMakeModel=function(){
+		return "`${make}` `${model}`"
+	}
+// class SportsCar extends Car{
+// 	constructor(make,model,topSpeed){
+// 		super(make,model);
+// 		this.topSpeed=topSpeed;
+// 	}
+// }
+SuperCar.prototype.getTopSpeed=function(){
+		return this.topSpeed;
+	}
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
