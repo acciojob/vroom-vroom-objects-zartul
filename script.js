c02aeb7acfee99ea3c2dbd06f8bfc357ea38c334
zartul <121471819+zartul@users.noreply.github.com>
2025-02-11 11:06:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function Car(make, model) {
 	this.model=model;
 }
 Car.prototype.getMakeModel=function(){
-		return "${this.make} ${this.model}";
+		return `${this.make} ${this.model}`;
 	}
 function SportsCar(make, model, topSpeed) {
 	Car.call(this,make,model);
